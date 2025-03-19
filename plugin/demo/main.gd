b64bcdfed39a115b4fd31c85efe97fa7d58f527a
RinXe <61826953+rinhxe@users.noreply.github.com>
2025-03-19 17:58:42 +0700

    DONE godot android plugin

--- plugin/demo/main.gd
+++ plugin/demo/main.gd
@@ -1,16 +1,65 @@
 extends Node2D
 
-# TODO: Update to match your plugin's name
 var _plugin_name = "GodotAndroidPluginTemplate"
 var _android_plugin
 
 func _ready():
 	if Engine.has_singleton(_plugin_name):
 		_android_plugin = Engine.get_singleton(_plugin_name)
+		_android_plugin.connect("file_selected", Callable(self, "_on_file_selected"))
+		_android_plugin.connect("file_selection_canceled", Callable(self, "_on_file_canceled"))
 	else:
 		printerr("Couldn't find plugin " + _plugin_name)
 
 func _on_Button_pressed():
 	if _android_plugin:
-		# TODO: Update to match your plugin's API
 		_android_plugin.helloWorld()
+		
+func _on_pick_file_press():
+	if _android_plugin:
+		_android_plugin.pickUpFile()
+
+func _on_file_selected(file_path: String, file_name: String):
+	print("File received:", file_path)
+	print("Original file name:", file_name)
+	
+	$Label.text = "Selected: " + file_name
+	
+	var new_file_path = "user://saved_files/" + file_name
+	
+	DirAccess.make_dir_recursive_absolute("user://saved_files")
+	
+	if FileAccess.file_exists(file_path):
+		if DirAccess.copy_absolute(file_path, new_file_path) == OK:
+			print("File saved as:", new_file_path)
+			_process_file(new_file_path)
+		else:
+			print("Failed to copy file")
+			$Label2.text = "Failed to copy file"
+	else:
+		print("File does not exist")
+		$Label2.text = "File does not exist"
+
+func _process_file(file_path: String):
+	if not FileAccess.file_exists(file_path):
+		print("File does not exist:", file_path)
+		$Label2.text = "Error: File does not exist!"
+		return
+	
+	var extension = file_path.get_extension().to_lower()
+	
+	print("Processing file:", file_path)
+	print("File extension:", extension)
+	$Label2.text = "Processing file: " + file_path + "\nExtension: " + extension
+	
+	if extension in ["txt", "json"]:
+		var file = FileAccess.open(file_path, FileAccess.READ)
+		if file:
+			var content = file.get_as_text()
+			print("File content:", content)
+			$Label2.text += "\nFile content:\n" + content
+		else:
+			print("Failed to open file")
+			$Label2.text += "\nFailed to open file"
+	else:
+		$Label2.text += "\nUnsupported file type"
--- plugin/src/main/java/org/godotengine/plugin/android/template/GodotAndroidPlugin.kt
+++ plugin/src/main/java/org/godotengine/plugin/android/template/GodotAndroidPlugin.kt
@@ -1,14 +1,9 @@
 // TODO: Update to match your plugin's package name.
 package org.godotengine.plugin.android.template
+import android.app.Activity
 import android.app.AlertDialog
-import android.app.DownloadManager
-import android.content.ContentValues
 import android.content.Intent
 import android.os.Environment
-import android.provider.MediaStore
-import android.util.Log
-import android.widget.Toast
-import androidx.core.content.FileProvider
 import org.godotengine.godot.Godot
 import org.godotengine.godot.plugin.GodotPlugin
 import org.godotengine.godot.plugin.UsedByGodot
@@ -16,6 +11,10 @@ import java.io.File
 import android.net.Uri
 import android.os.Build
 import android.provider.DocumentsContract
+import android.widget.Toast
+import org.godotengine.godot.plugin.SignalInfo
+import java.io.FileOutputStream
+import java.io.InputStream
 
 class GodotAndroidPlugin(godot: Godot): GodotPlugin(godot) {
 
@@ -26,20 +25,15 @@ class GodotAndroidPlugin(godot: Godot): GodotPlugin(godot) {
      *
      * Shows a 'Hello World' toast.
      */
+    //region OpenFolder
     @UsedByGodot
     fun helloWorld() {
         runOnUiThread {
-            //Toast for test
-            //Toast.makeText(activity, "Hello World", Toast.LENGTH_LONG).show()
-            //Log.v(pluginName, "Hello World")
-            //Test intent
-            /*val intent = Intent(DownloadManager.ACTION_VIEW_DOWNLOADS)
-            activity?.startActivity(intent)*/
-
             saveFolder()
             openFolder()
         }
     }
+
 
     @UsedByGodot
     fun showAlert(title: String, message: String) {
@@ -121,4 +115,82 @@ class GodotAndroidPlugin(godot: Godot): GodotPlugin(godot) {
             showAlert("Error", e.message ?: "Unknown error")
         }
     }
+    companion object {
+        private const val FILE_PICKER_REQUEST_CODE = 1001
+    }
+    //endregion
+    //region PickUpFile
+    @UsedByGodot
+    fun pickUpFile() {
+        activity?.runOnUiThread {
+            Toast.makeText(activity, "You are picking a file", Toast.LENGTH_LONG).show()
+
+            val intent = Intent(Intent.ACTION_GET_CONTENT).apply {
+                type = "*/*"
+                addCategory(Intent.CATEGORY_OPENABLE)
+            }
+
+            activity?.startActivityForResult(intent, FILE_PICKER_REQUEST_CODE)
+        }
+    }
+
+    override fun onMainActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == FILE_PICKER_REQUEST_CODE) {
+            if (resultCode == Activity.RESULT_OK && data?.data != null) {
+                val uri = data.data!!
+                val fileName = getFileName(uri)
+                val file = saveFileToInternalStorage(uri, fileName)
+
+                if (file != null) {
+                    emitSignal("file_selected", file.absolutePath, fileName)
+                    Toast.makeText(activity, "Emit file selected", Toast.LENGTH_LONG).show()
+                } else {
+                    emitSignal("file_selection_canceled")
+                    Toast.makeText(activity, "Emit file not selected", Toast.LENGTH_LONG).show()
+                }
+            } else {
+                emitSignal("file_selection_canceled")
+                Toast.makeText(activity, "Emit file not selected", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun saveFileToInternalStorage(uri: Uri, fileName: String): File? {
+        return try {
+            val inputStream: InputStream? = activity?.contentResolver?.openInputStream(uri)
+            val file = File(activity?.filesDir, fileName) // Lưu file với đúng tên gốc
+            val outputStream = FileOutputStream(file)
+
+            inputStream?.copyTo(outputStream)
+
+            inputStream?.close()
+            outputStream.close()
+            file
+        } catch (e: Exception) {
+            null
+        }
+    }
+
+    private fun getFileName(uri: Uri): String {
+        var name = "unknown_file"
+        activity?.contentResolver?.query(uri, null, null, null, null)?.use { cursor ->
+            if (cursor.moveToFirst()) {
+                val index = cursor.getColumnIndex("_display_name")
+                if (index != -1) {
+                    name = cursor.getString(index)
+                }
+            }
+        }
+        return name
+    }
+
+    override fun getPluginSignals(): MutableSet<SignalInfo> {
+        return mutableSetOf(
+            SignalInfo("file_selected", String::class.java, String::class.java), // Trả về cả đường dẫn và tên file
+            SignalInfo("file_selection_canceled")
+        )
+    }
+//endregion
+
+
 }
